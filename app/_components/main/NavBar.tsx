22d807cdc9247c43db2cffb192d711065a7eaf30
Younes Elaomari <youneselaomari@gmail.com>
2024-12-24 23:18:47 +0000

    Ref removing the fixed navbar

--- app/_components/main/NavBar.tsx
+++ app/_components/main/NavBar.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <motion.nav initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <div className="h-[55px] fixed top-[10px] rounded-lg right-[10px] left-[10px] shadow-lg shadow-[#2a0e61]/50 bg-[#010125]  z-50 md:px-5 px-2">
+      <div className="h-[55px] shadow-lg shadow-[#2a0e61]/50 bg-[#010125] md:px-10 px-5">
         <div className="w-full h-full flex flex-row items-center justify-between px-[10px] max-w-[1536px] m-auto">
           <Link
             href="/"
